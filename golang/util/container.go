ef22fa8381d5506dbe91b2269b51482ddb31249b
Mike Brown <github@torvosoft.com>
2022-10-20 14:31:10 -0400

    Add container digest utilities
    This change adds a couple of container utility functions to assist in
    determining whether a local container image should be updated or not
    with an image pull.

--- golang/util/container.go
+++ golang/util/container.go
@@ -2,24 +2,41 @@ package util
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/mount"
+	"github.com/docker/docker/client"
 	dockerClient "github.com/docker/docker/client"
 	"github.com/docker/docker/pkg/stdcopy"
 )
 
 const defaultRetries = 100
 
-const BopmaticBuildImageName = "bopmatic/build:latest"
-const DockerInstallErrMsg = "Could not invoke docker; please double check that you have docker installed: %w"
+const (
+	BopmaticImageRepo      = "bopmatic/build"
+	BopmaticImageTag       = "latest"
+	BopmaticBuildImageName = BopmaticImageRepo + ":" + BopmaticImageTag
+
+	DockerInstallErrMsg = "Could not invoke docker; please double check that you have docker installed: %w"
+)
+
+var ErrImageNotFound = errors.New("image does not exist")
 
 func HasBopmaticBuildImage() (bool, error) {
+	return HasImage(BopmaticImageRepo, BopmaticImageTag)
+}
+
+func HasImage(repository string, tag string) (bool, error) {
 	cli, err := dockerClient.NewClientWithOpts(dockerClient.FromEnv,
 		dockerClient.WithAPIVersionNegotiation())
 	if err != nil {
@@ -37,13 +54,117 @@ func HasBopmaticBuildImage() (bool, error) {
 
 	for _, img := range images {
 		for _, tag := range img.RepoTags {
-			if tag == BopmaticBuildImageName {
+			if tag == repository+":"+tag {
 				return true, nil
 			}
 		}
 	}
 
 	return false, nil
+}
+
+func GetLocalImageDigest(repository string, tag string) (string, error) {
+	cli, err := dockerClient.NewClientWithOpts(dockerClient.FromEnv,
+		dockerClient.WithAPIVersionNegotiation())
+	if err != nil {
+		return "", fmt.Errorf(DockerInstallErrMsg, err)
+	}
+
+	ctx := context.Background()
+	ctx, cancelFunc := context.WithTimeout(ctx, time.Second*5)
+	defer cancelFunc()
+
+	imgInspect, _, err := cli.ImageInspectWithRaw(ctx, repository+":"+tag)
+	if err != nil {
+		if client.IsErrNotFound(err) {
+			err = fmt.Errorf("Could not find local image digest for %v:%v: %w",
+				repository, tag, ErrImageNotFound)
+		}
+		return "", err
+	}
+
+	for _, digest := range imgInspect.RepoDigests {
+		// cannot find documentation on RepoDigests content; just guessing
+		// here that there may be potentially more digest checksums than sha256
+		if strings.Contains(digest, "sha256") &&
+			strings.Contains(digest, repository) {
+			// digest format here differs from that of GetRemoteImageDigest()
+			// the local digest includes a repo w/ @ prefix so strip that here
+			digestParts := strings.Split(digest, "@")
+			if len(digestParts) != 2 {
+				return "", fmt.Errorf("Could not parse local digest %v", digest)
+			}
+			return digestParts[1], nil
+		}
+	}
+
+	err = fmt.Errorf("Could not find local image digest for %v:%v: %w",
+		repository, tag, ErrImageNotFound)
+
+	return "", err
+}
+
+// @todo investigate whether this is achievable with github.com/docker/docker
+// rather than writing our own custom json parsing logic
+func GetRemoteImageDigest(repository string, tag string) (string, error) {
+	// note TagsResult&TagsResponse as defined here do not capture the complete
+	// set of information returned from the tagsURL; we only define the minimum
+	// subset of fields we need to extract the image digest
+	type TagsResult struct {
+		Name   string `json:"name"`
+		Digest string `json:"digest"`
+	}
+	type TagsResponse struct {
+		Count   int          `json:"count"`
+		Results []TagsResult `json:"results"`
+	}
+
+	tagsURL := fmt.Sprintf("https://registry.hub.docker.com/v2/repositories/%v/tags",
+		repository)
+
+	client := http.Client{
+		Timeout: time.Second * 30,
+	}
+
+	resp, err := client.Get(tagsURL)
+	if err != nil {
+		return "", err
+	}
+
+	tagsJsonDoc, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	var tagsResponse TagsResponse
+	err = json.Unmarshal(tagsJsonDoc, &tagsResponse)
+	if err != nil {
+		return "", err
+	}
+
+	for _, tagResult := range tagsResponse.Results {
+		if tagResult.Name == tag {
+			return tagResult.Digest, nil
+		}
+	}
+
+	return "", fmt.Errorf("Could not find tag %v for repo %v at %v: %w", tag,
+		repository, tagsURL, ErrImageNotFound)
+}
+
+func DoesLocalImageNeedUpdate(repository string, tag string) (bool, error) {
+	localDigest, err := GetLocalImageDigest(repository, tag)
+	if errors.Is(err, ErrImageNotFound) {
+		return true, nil
+	} else if err != nil {
+		return false, err
+	}
+	remoteDigest, err := GetRemoteImageDigest(repository, tag)
+	if err != nil {
+		return false, err
+	}
+
+	return localDigest != remoteDigest, nil
 }
 
 func RunContainerCommand(ctx context.Context, cmdAndArgs []string,
---
+++ golang/util/container_test.go
@@ -0,0 +1,52 @@
+package util
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestContainerUtils(t *testing.T) {
+	hasImage, err := HasImage("foo", "bar")
+	if err != nil {
+		t.Errorf("HasImage(foo,bar) failed: %v", err)
+	} else if hasImage {
+		t.Errorf("HasImage(foo,bar) failed to return false")
+	}
+	_, err = GetLocalImageDigest("foo", "bar")
+	if !errors.Is(err, ErrImageNotFound) {
+		t.Errorf("GetRemoteImageDigest(foo,bar) failed to return ErrImageNotFound: %v",
+			err)
+	}
+
+	remoteDigest, err := GetRemoteImageDigest("bopmatic/build", "latest")
+	if err != nil {
+		t.Errorf("Failed to get remote digest: %v", err)
+	}
+	t.Logf("Remote digest is %v", remoteDigest)
+
+	localDigest, err := GetLocalImageDigest("bopmatic/build", "latest")
+	var wasNotFound bool
+	if errors.Is(err, ErrImageNotFound) {
+		wasNotFound = true
+		t.Logf("Local digest was not found")
+	} else if err != nil {
+		t.Errorf("Failed to get local digest: %v", err)
+	} else {
+		t.Logf("Local digest is  %v", localDigest)
+	}
+
+	needUpdate, err := DoesLocalImageNeedUpdate("bopmatic/build", "latest")
+	if err != nil {
+		t.Errorf("DoesLocalImageNeedUpdate() failed unexpectedly: %v", err)
+	} else if wasNotFound && !needUpdate {
+		t.Errorf("DoesLocalImageNeedUpdate() should have returned true")
+	} else {
+		if needUpdate && localDigest == remoteDigest {
+			t.Errorf("DoesLocalImageNeedUpdate() should have returned false")
+		}
+		if !needUpdate && localDigest != remoteDigest {
+			t.Errorf("DoesLocalImageNeedUpdate() should have returned true")
+		}
+	}
+
+}
